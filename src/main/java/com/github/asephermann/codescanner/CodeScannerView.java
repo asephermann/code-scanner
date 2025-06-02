2ce8d6379449dbb3592b8ac3785d1a3b3c3ccd48
Nursalam <35249316+asephermann@users.noreply.github.com>
2025-06-02 16:53:37 +0700

    Update CodeScannerView.java

--- src/main/java/com/github/asephermann/codescanner/CodeScannerView.java
+++ src/main/java/com/github/asephermann/codescanner/CodeScannerView.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.budiyev.android.codescanner;
+package com.github.asephermann.codescanner;
 
 import java.util.Objects;
 
